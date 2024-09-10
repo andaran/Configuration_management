2d9f58951c5dc31f0e7ec4b65f784fb0e6b65fb5
Andrey <andrey@vl-novikov.ru>
2024-09-10 18:16:00 +0300

    Добавлены разные цвета в консоль

--- Task_1/src/console.py
+++ Task_1/src/console.py
@@ -5,6 +5,8 @@ class Console:
     bg_color = "#2e2e2e"
     text_color = "#ffffff"
     prompt_color = "#00ff00"
+    prompt_user_color = "#00ff00"
+    prompt_path_color = "#00bfff"
 
     def __init__(self, cmd_callback):
         self.cmd_callback = cmd_callback
@@ -19,6 +21,9 @@ class Console:
         self.console.grid(row=0, column=0, padx=0, pady=0)
         self.console.bind('<Return>', self.execute_command)
         self.console.config(font=("Courier New", 12))
+
+        self.console.tag_configure("user", foreground=self.prompt_user_color)
+        self.console.tag_configure("path", foreground=self.prompt_path_color)
 
     def execute_command(self, event):
         input_text = self.console.get("1.0", tk.END)
@@ -39,7 +44,9 @@ class Console:
         self.console.insert(tk.END, f"{text}\n")
 
     def insert_prompt(self):
-        self.console.insert(tk.END, f"user@computer:{self.path}$ ")
+        self.console.insert(tk.END, "user@computer", "user")
+        self.console.insert(tk.END, f":{self.path}", "path")
+        self.console.insert(tk.END, "$ ")
         self.console.mark_set("insert", tk.END)
 
     def set_path(self, path):
