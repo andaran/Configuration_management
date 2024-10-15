09aa43c42543667e00c0e8d9a24eb8f3c5226339
andaran <andrnovip@gmail.com>
2024-10-15 18:26:37 +0300

    Добавлен базовый функционал

--- Task_3/main.py
+++ Task_3/main.py
@@ -1,9 +1,89 @@
 import re
 import sys
 
+def parse_array(nesting, array_str):
+    array_str = array_str.strip()
+
+    if not array_str:
+        return None
+    if array_str[0] != "[" or array_str[-1] != "]":
+        return None
+    
+    array_str = array_str[1:-1]
+    result = "\n"
+    for elem in array_str.split():
+        if not elem:
+            return None
+        result += f"{'  ' * (nesting + 1)}- {elem}\n"
+
+    return result
+
+def parse_value(value):
+    pass
+
 def parse_config(file_content):
-    constants = {}
     lines = file_content.splitlines()
+    result = ""
+    nesting = 0
+    is_dict = False
+
+    for num, line in enumerate(lines):
+        line = line.strip()
+
+        if nesting > 0:
+            is_dict = True
+        else:
+            is_dict = False
+
+        # Пустая строка
+        if not line:
+            continue
+
+        # Комментарий
+        if line.startswith('!'):
+            result += f"{'  ' * nesting}# {line[1:].strip()}\n"
+
+        # Константы
+        const_match = re.match(r"^[a-zA-Z]+", line)
+        if const_match:
+            name = const_match.group()
+
+            if not is_dict and line[len(name)] != ":":
+                raise ValueError(f"Неверный формат константы: {line} (строка {num + 1})")
+            if is_dict and line[len(name):len(name) + 2] != " =":
+                raise ValueError(f"Неверный формат элемента словаря: {line} (строка {num + 1})")
+
+            if is_dict:
+                value = line[len(name) + 2:].strip()
+            else:
+                value = line[len(name) + 1:].strip()
+
+            if value == "":
+                raise ValueError(f"Пустое значение константы: {line} (строка {num + 1})")
+            else:
+                result += f"{'  ' * nesting}{name}:"
+
+            if value == "{":
+                result += "\n"
+                nesting += 1
+            elif value.startswith("["):
+                array = parse_array(nesting, value)
+                if array is None:
+                    raise ValueError(f"Неверный формат массива: {line} (строка {num + 1})")
+                result += array
+            else:
+                if not value.isdigit():
+                    raise ValueError(f"Неверный формат числа: {line} (строка {num + 1})")
+                result += f" {value}\n"
+            continue
+
+        # Закрытие словаря
+        if line == "}":
+            nesting -= 1
+            continue
+
+    return result
+
 
 def main(file_path):
     try:
